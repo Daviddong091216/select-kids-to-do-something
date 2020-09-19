fa35d5c3d72dbf297273fb8210ec421f5991b65d
David Dong <daviddong201712@gmail.com>
2020-09-19 00:40:45 -0500

    Display the diteals of the selected kid.

--- src/app/kids/kids.component.ts
+++ src/app/kids/kids.component.ts
@@ -8,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class KidsComponent implements OnInit {
   missionName : string ='housework'
   kids = [
-    {name: 'Liano', data: {age: 12, mass: '45 kg', sidekick: 'Liano'}, image: 'assets/images/Liano.png'},
-    {name: 'Ethan', data: {age: 11, mass: '50 kg', sidekick: 'Ethan'}, image: 'assets/images/Ethan.png'},
-    {name: 'Diamen', data: {age: 9, mass: '40 kg', sidekick: 'Diamen'}, image: 'assets/images/Diamen.png'},
-    {name: 'Miley', data: {age: 8, mass: '30 kg', sidekick: 'Miley'}, image: 'assets/images/Miley.png'},
-    {name: 'Aaron', data: {age: 4, mass: '10 kg', sidekick: 'Aaron'}, image: 'assets/images/Aaron.png'}
+    {name: 'Liano', data: {age: 12, mass: '45 kg', sidekick: 'Emma'}, image: 'assets/images/Liano.png'},
+    {name: 'Ethan', data: {age: 11, mass: '50 kg', sidekick: 'Sean'}, image: 'assets/images/Ethan.png'},
+    {name: 'Diamen', data: {age: 9, mass: '40 kg', sidekick: 'Dan'}, image: 'assets/images/Diamen.png'},
+    {name: 'Miley', data: {age: 8, mass: '30 kg', sidekick: 'Emily'}, image: 'assets/images/Miley.png'},
+    {name: 'Aaron', data: {age: 4, mass: '10 kg', sidekick: 'Mike'}, image: 'assets/images/Aaron.png'}
   ];
   constructor() { }
 
